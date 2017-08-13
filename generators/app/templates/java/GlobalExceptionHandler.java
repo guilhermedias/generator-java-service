87f1919209a7525a463e129288e2a8cc82824f97
William Ahrons <william.ahrons@gmail.com>
2017-08-12 22:39:42 -0300

    Add GlobalExceptionHandler to provide generic Handlers #29 (#36)
    Why this change is needed?
    With this commit, we are introducing a global exception handler that
    will give for us a handler for spring exceptions. Instead of always sent
    a 500 error, with this it will address the correct handler for each type
    of exception.
    
    How this commit address this issue?
    It adds a ControllerAdvice in the application extendig a generic
    exception handler provided by Spring.

---
+++ generators/app/templates/java/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package <%=defaultPackage%>.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {}
